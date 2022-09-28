eabc10ff6310eff20d54b762b51056ef4a4566ec
Tyler-251 <tyler.wayne2002@hotmail.com>
2022-09-27 23:21:18 -0500

    projects init

---
+++ src/components/ProjectsMain.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const ProjectsMain = (props) => {
+    return (
+        <div class="main">
+            t
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { AppHeader } from "./components/AppHeader.js"
 import { NavBar } from "./components/NavBar.js"
 import { AboutMain } from "./components/AboutMain.js"
 import { InfoMain } from "./components/InfoMain.js"
-import { Error } from "./components/Error.js"
-import { Footer } from "./components/Footer.js"
 import { HomeMain } from "./components/HomeMain.js"
+import { ProjectsMain } from "./components/ProjectsMain.js"
+import { Footer } from "./components/Footer.js"
+import { Error } from "./components/Error.js"
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,6 +19,9 @@ switch (window.location.pathname) {
         break;
     case "/info":
         MainDiv = InfoMain;
+        break;
+    case "/projects":
+        MainDiv = ProjectsMain;
         break;
     case "/about":
         MainDiv = AboutMain;
