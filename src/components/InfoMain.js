a6d584b4a5b06b7c5c85637fb4f9edf35a1e3087
Tyler-251 <tyler.wayne2002@hotmail.com>
2022-09-30 16:24:27 -0500

    resume fix 2

--- src/components/InfoMain.js
+++ src/components/InfoMain.js
@@ -1,17 +1,16 @@
 // #region imports
 import React, { useState } from "react"
 import { Document, Page, pdfjs } from 'react-pdf';
-import resume from "../data/resume.pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 // #endregion
 export const InfoMain = (props) => {
     return (
         <div class="main">
             <div class="infomain">
-                <a class="btn" href={"/^/" + resume} target="_blank" rel="noreferrer">Open Resume &rarr;</a>
+                <a class="btn" href="/resume.pdf" target="_blank" rel="noreferrer">Open Resume &rarr;</a>
             </div>
             <div class="document">
-                <Document file={"/^/" + resume}>
+                <Document file="/resume.pdf">
                     <Page pageNumber={1}/>
                 </Document>
             </div>
